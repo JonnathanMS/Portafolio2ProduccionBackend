69b29a281aa4b095db9b8f5a81060f2b7114a0be
JonnathanMS <jhonnnario@gmail.com>
2023-11-21 09:39:04 -0500

    vercel

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,6 +1,13 @@
 import app from './app.js' // en app.js estamos ejecutando express que es el servidor
 import { connectDB } from "./db.js";
 
+const port = process.env.PORT || 4000;
 connectDB(); // se ejecuta la conexion a la base de datos.
-app.listen(4000);
-console.log('Server on port ', 4000);
+
+app.listen(port, () => {
+    console.log(`puerto corriendo en: ${port}`);
+})
+
+// app.listen(4000);
+// console.log('Server on port ', 4000);
+
