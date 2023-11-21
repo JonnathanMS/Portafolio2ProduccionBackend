ef277a68fb123871bcdd296546be365a1d9e025d
JonnathanMS <jhonnnario@gmail.com>
2023-11-21 12:07:10 -0500

    Se configura vercel

--- Backend/index.js
+++ Backend/index.js
@@ -1,5 +1,5 @@
-import app from './app.js' // en app.js estamos ejecutando express que es el servidor
-import { connectDB } from "./db.js";
+import app from './src/app.js' // en app.js estamos ejecutando express que es el servidor
+import { connectDB } from "./src/db.js";
 
 const port = process.env.PORT || 4000;
 connectDB(); // se ejecuta la conexion a la base de datos.
@@ -8,6 +8,9 @@ app.listen(port, () => {
     console.log(`puerto corriendo en: ${port}`);
 })
 
+app.get("/", (req, res) => {
+    res.send("Esta activo el backend")
+})
 // app.listen(4000);
 // console.log('Server on port ', 4000);
 
