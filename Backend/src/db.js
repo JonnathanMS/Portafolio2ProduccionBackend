f456013cc92a11d211e4d50eb6229b3a47531be4
JonnathanMS <jhonnnario@gmail.com>
2023-11-21 21:03:47 -0500

    archivo env

--- Backend/src/db.js
+++ Backend/src/db.js
@@ -5,6 +5,9 @@ export const connectDB = async () => {
     try {
         await mongoose.connect(`mongodb+srv://jonnathanmonroybd:${TOKEN_SECRET}@cluster0.j8kwwty.mongodb.net/?retryWrites=true&w=majority`);
         console.log("MongoDB is connected");
+        router.get("/", (req, res) => {
+            res.send("Esta activo el backend")
+        })
     } catch (error) {
         console.error(error);
     }
