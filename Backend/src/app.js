87a7e667859495e64dd27ebb46779ea9988d4e79
JonnathanMS <jhonnnario@gmail.com>
2023-11-22 20:35:15 -0500

    render

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,7 +1,6 @@
 import express, { application } from 'express';
 import morgan from 'morgan';
 import cookieParser from 'cookie-parser';
-import bodyParser from 'body-parser';
 import authRoutes from './routes/auth.routes.js';
 import taskRoutes from './routes/tasks.routes.js';
 import citasGeneralRoutes from './routes/citasGeneral.routes.js';
@@ -9,7 +8,6 @@ import cors from 'cors';
 import CitaGeneral from "./models/citaGeneral.model.js";
 
 const app = express(); // es basicamente el servidor
-app.use(bodyParser.json);
 // app.listen(3000); esto se provo y se paso para index
 // console.log('on port ', 3000);
 // app.use(cors()); // permite que cualquier dominio se puedea comunicar en este servidor
@@ -28,7 +26,7 @@ app.use(cookieParser());
 app.use("/api/v1", authRoutes);// asi se modifica la url// app.use(authRoutes); asi cuando api se pone desde auth.routes.js
 app.use("/api/v1", taskRoutes);
 app.use("/api/v1", citasGeneralRoutes);
-app.use("/", async (req, res) => {
+app.use("/", (req, res) => {
     res.send("Api funcionando desde app");
 })
 
